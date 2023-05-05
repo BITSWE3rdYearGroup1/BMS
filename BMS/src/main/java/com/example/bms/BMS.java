a19dc2a90946eef9d2c9a2920925e487e851bc76
akalewoled <fikerbdu@gmail.com>
2023-05-05 15:08:49 +0200

    checking my participation

--- BMS/src/main/java/com/example/bms/BMS.java
+++ BMS/src/main/java/com/example/bms/BMS.java
@@ -1,5 +1,5 @@
 package com.example.bms;
-
+//this is to check fikiremariams conttibute to th git hub
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Group;
--- BMS/src/main/java/module-info.java
+++ BMS/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.bms {
     requires javafx.controls;
     requires javafx.fxml;
-
+                                                 
 
     opens com.example.bms to javafx.fxml;
     exports com.example.bms;
